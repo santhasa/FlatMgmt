369e0b0d4602d864b90e73f7b27903aa122d5ee3
santhasa <sankar@tickingminds.com>
2014-11-13 13:23:53 +0530

    error msg changed

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -7,16 +7,20 @@ $(function(){
                 //change the url for your project
                 url: 'http://192.168.1.145:8888/flatmgmt/php/login.php',
                 success: function(data){
-                	alert(eval(data));
+                	//alert(eval(data));
                     if(eval(data)=="Wrong Password") {
-		                    document.getElementById("wrongPwd").style.visibility="visible";
-                    		document.getElementById("wrongEmail").style.visibility="hidden";
-                    		document.getElementById("techError").style.visibility="hidden";
+		                    document.getElementById("errorMsg").style.visibility="visible";
+		                    document.getElementById("errorMsg").innerHTML="You seem to have entered a wrong password";
+		                    //document.getElementById("wrongPwd").style.visibility="visible";
+                    		//document.getElementById("wrongEmail").style.visibility="hidden";
+                    		//document.getElementById("techError").style.visibility="hidden";
                     }
                     else if(eval(data)=="E-mail does not exist") {
-		                    document.getElementById("wrongPwd").style.visibility="hidden";
-                    		document.getElementById("wrongEmail").style.visibility="visible";
-                    		document.getElementById("techError").style.visibility="hidden";
+		                    document.getElementById("errorMsg").style.visibility="visible";
+		                    document.getElementById("errorMsg").innerHTML="Your e-mail does not seem to exist";
+		                    //document.getElementById("wrongPwd").style.visibility="hidden";
+                    		//document.getElementById("wrongEmail").style.visibility="visible";
+                    		//document.getElementById("techError").style.visibility="hidden";
 
                     }
                     else {
@@ -24,9 +28,11 @@ $(function(){
 	                }
                 },
                 error: function(){
-		                    document.getElementById("wrongPwd").style.visibility="hidden";
-                    		document.getElementById("wrongEmail").style.visibility="hidden";
-                    		document.getElementById("techError").style.visibility="visible";
+		                    document.getElementById("errorMsg").style.visibility="visible";
+		                    document.getElementById("errorMsg").innerHTML="There seems to be a technical error. Please try again later.";
+		                    //document.getElementById("wrongPwd").style.visibility="hidden";
+                    		//document.getElementById("wrongEmail").style.visibility="hidden";
+                    		//document.getElementById("techError").style.visibility="visible";
 
                 }
             });
